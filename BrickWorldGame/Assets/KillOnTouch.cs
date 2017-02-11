c73192c3917b4a9c7658c62a5ed8482cb15636ef
ndileas <ndileas@gmail.com>
2017-02-11 13:06:57 -0500

    Added static spike and kill script to project.

---
+++ BrickWorldGame/Assets/KillOnTouch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnTouch : MonoBehaviour {
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // assuming "Player"
+
+        // check if collison is with player
+        if (collision.collider.tag == "Player") {
+            // kill player
+            Player p;
+
+            p = collision.collider.GetComponent<Player>();
+
+            p.kill();
+        }
+
+        return;
+    }
+}
