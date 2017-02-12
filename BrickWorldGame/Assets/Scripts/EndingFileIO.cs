7ccd3a67e7c3fd31ba8c279ef827c70f5e695ace
siuster95 <jxs7205@rit.edu>
2017-02-11 20:12:58 -0500

    Endingscene and the File IO script for it

---
+++ BrickWorldGame/Assets/Scripts/EndingFileIO.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class EndingFileIO : MonoBehaviour {
+
+    private StreamReader SR;
+    private StreamWriter SW;
+    private Dictionary<string, int> Dic;
+    private int counter;
+	// Use this for initialization
+	void Start ()
+    {
+        counter = 0;
+        Dic = new Dictionary<string, int>();
+        if (File.Exists("Assets/text_file/highscore.txt"))
+        {
+            SR = new StreamReader("Assets/text_file/highscore.txt");
+            string line;
+            string name;
+            int score;
+            while ((line = SR.ReadLine()) != null)
+            {
+                string[] ListofNameandNumber = line.Split(' ');
+                name = ListofNameandNumber[0];
+                score = int.Parse(ListofNameandNumber[1]);
+                Dic.Add(name, score);
+            }
+            SR.Close();
+        }
+        else
+        {
+            SW = new StreamWriter("Assets/text_file/highscore.txt");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        foreach (KeyValuePair<string, int> entry in Dic)
+        {
+            
+        }
+    }
+}
