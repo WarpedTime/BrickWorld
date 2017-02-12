1cc0b01ae9f24425ab913e0399cbe6531ed67449
WarpedTime <silverstorm79@gmail.com>
2017-02-12 02:19:24 -0500

    made game loop

--- BrickWorldGame/Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
+++ BrickWorldGame/Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ScrollingUVs_Layers : MonoBehaviour 
 {
 	//public int materialIndex = 0;
-	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
+	[SerializeField] public Vector2 uvAnimationRate = new Vector2( 10.0f, 0.0f );
 	public string textureName = "_MainTex";
 	
 	Vector2 uvOffset = Vector2.zero;
