bb442422965f5c5c0412c5969dea18397ece72d0
siuster95 <jxs7205@rit.edu>
2017-02-12 00:32:22 -0500

    endingscreen should work

--- BrickWorldGame/Assets/Scripts/EndingFileIO.cs
+++ BrickWorldGame/Assets/Scripts/EndingFileIO.cs
@@ -2,45 +2,122 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
+using System.Linq;
 
 public class EndingFileIO : MonoBehaviour {
 
     private StreamReader SR;
     private StreamWriter SW;
-    private Dictionary<string, int> Dic;
-    private int counter;
+    private List<KeyValuePair<string, int>> Dic;
+    private int counter,lineread;
+    private Text[] names;
+    private Text[] scores;
+    private List<int> scoresordering;
+    private Button button;
+    private int playerscorewon;
+    private GameManagerScript GM;
 	// Use this for initialization
 	void Start ()
     {
-        counter = 0;
-        Dic = new Dictionary<string, int>();
-        if (File.Exists("Assets/text_file/highscore.txt"))
+        //initiaze text arrays to hold text gameobjects
+        names = new Text[3];
+        scores = new Text[3];
+        scoresordering = new List<int>();
+        Dic = new List<KeyValuePair<string, int>>();
+        button = GameObject.Find("EnterButton").GetComponent<Button>();
+        button.onClick.AddListener(EnterName);
+        GM = GameObject.Find("GM").GetComponent<GameManagerScript>();
+        playerscorewon = GM.Score;
+        GameObject.Find("PlayerScore").GetComponent<Text>().text = playerscorewon.ToString();
+        for (int x =1;x<4;x++)
         {
-            SR = new StreamReader("Assets/text_file/highscore.txt");
-            string line;
-            string name;
-            int score;
-            while ((line = SR.ReadLine()) != null)
-            {
-                string[] ListofNameandNumber = line.Split(' ');
-                name = ListofNameandNumber[0];
-                score = int.Parse(ListofNameandNumber[1]);
-                Dic.Add(name, score);
-            }
-            SR.Close();
+            names[x-1] = GameObject.Find("Name" + x).GetComponent<Text>();
+            scores[x-1] = GameObject.Find("Score" + x).GetComponent<Text>();
         }
-        else
-        {
-            SW = new StreamWriter("Assets/text_file/highscore.txt");
-        }
+        this.makeleaderboard();
+       
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        foreach (KeyValuePair<string, int> entry in Dic)
+       
+    }
+
+
+
+    public void EnterName()
+    {
+        string name = GameObject.Find("InputField").GetComponent<InputField>().text;
+        if (name != "")
         {
-            
+            SW = new StreamWriter("Assets/text_file/highscore.txt",true);
+            string score = GameObject.Find("PlayerScore").GetComponent<Text>().text;
+            SW.WriteLine(name + " " + score);
+            SW.Close();
+            GameObject.Find("EnterText").GetComponent<Text>().text = "Quit";
+            this.makeleaderboard();
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(quitevent);
+        }
+    }
+
+    public void quitevent()
+    {
+        Application.Quit();
+    }
+
+    public void makeleaderboard()
+    {
+        counter = 1;
+        lineread = 0;
+        if (File.Exists("Assets/text_file/highscore.txt"))
+        {
+            //open the file
+            SR = new StreamReader("Assets/text_file/highscore.txt");
+            //initalize temps vars
+            string line;
+            string name;
+            int score;
+            //read line by line and place into dictionary
+            Dic.Clear();
+            while ((line = SR.ReadLine()) != null && line != "")
+            {
+                
+                string[] ListofNameandNumber = line.Split(' ');
+                name = ListofNameandNumber[0];
+                score = int.Parse(ListofNameandNumber[1]);
+                Dic.Add(new KeyValuePair<string, int>(name, score));
+            }
+            SR.Close();
+
+
+
+            //order it 
+            Dic.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+            Dic.Reverse();
+            //find the value and put it in the new dic
+
+
+            //read the top 3
+            foreach (KeyValuePair<string, int> something in Dic)
+            {
+                if (counter < 4)
+                {
+                    string namein = something.Key;
+                    int scorein = something.Value;
+                    names[counter - 1].text = namein;
+                    scores[counter - 1].text = scorein.ToString();
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
         }
     }
 }
