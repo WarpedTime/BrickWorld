42e86eb74e0a1edb738c4e9458cfa034adacd09e
axl8178 <axl8178@rit.edu>
2017-02-11 23:08:01 -0500

    its a me ario

---
+++ BrickWorldGame/Assets/Scripts/TheWall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonPressed()
+    {
+        Debug.Log("The Gate is Open");
+        gameObject.SetActive(false);
+    }
+}
---
+++ BrickWorldGame/Assets/Scripts/botton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botton : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("touched");
+        if (other.tag == "Player")
+        {
+            TheWall wall;
+            wall = GameObject.Find("Cube").GetComponent<TheWall>();
+            wall.ButtonPressed();
+        }
+    }
+}
